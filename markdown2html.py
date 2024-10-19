04f9c96df07525ff4ea29ac4b1f8c7bbdd18c307
HindZiki <hind23051@gmail.com>
2024-10-19 08:23:40 +0000

    1

--- markdown2html.py
+++ markdown2html.py
@@ -17,3 +17,11 @@ if __name__ == '__main__':
     if not os.path.isfile(sys.argv[1]):
         print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
         exit(1)
+
+        length = len(line)
+headings = line.lstrip('#')
+heading_num = length - len(headings)
+
+# Headings parsing
+if 1 <= heading_num <= 6:
+    line = '<h{}>'.format(heading_num) + headings.strip() + '</h{}>\n'.format(heading_num)
